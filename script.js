ed3f0c30f1321942eebc705c8ee4f2374964b5a2
Hpayeur <hailpaye017@gmail.com>
2026-01-21 17:27:10 -0700

    Staying with a function that works

--- script.js
+++ script.js
@@ -19,36 +19,3 @@ document.getElementById("generateBtn").addEventListener("click", function () {
   const password = generatePassword(length);
   document.getElementById("passwordOutput").value = password;
 });
-
-// Testing This Function
-// function generatePassword() {
-//     const length = 12; // Or get from user input
-//     const includeUppercase = true; // From checkbox
-//     const includeLowercase = true; // From checkbox
-//     const includeNumbers = true; // From checkbox
-//     const includeSymbols = true; // From checkbox
-
-//     const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     const lower = 'abcdefghijklmnopqrstuvwxyz';
-//     const numbers = '0123456789';
-//     const symbols = '!@#$%^&*()_+';
-
-//     let allChars = '';
-//     let password = '';
-
-//     if (includeUppercase) { allChars += upper; password += upper[Math.floor(Math.random() * upper.length)]; }
-//     if (includeLowercase) { allChars += lower; password += lower[Math.floor(Math.random() * lower.length)]; }
-//     if (includeNumbers) { allChars += numbers; password += numbers[Math.floor(Math.random() * numbers.length)]; }
-//     if (includeSymbols) { allChars += symbols; password += symbols[Math.floor(Math.random() * symbols.length)]; }
-
-//     // Fill the rest of the password length
-//     while (password.length < length) {
-//         const randomChar = allChars[Math.floor(Math.random() * allChars.length)];
-//         password += randomChar;
-//     }
-
-//     // Shuffle the final password (simplified shuffle)
-//     // For better randomness, use a proper shuffle algorithm or crypto [5]
-//     const shuffledPassword = password.split('').sort(() => 0.5 - Math.random()).join('');
-//     return shuffledPassword;
-// }
